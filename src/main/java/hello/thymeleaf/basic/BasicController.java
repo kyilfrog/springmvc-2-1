74128db754da13f6a098546302e29a4b43dce3ae
kyilfrog <160857010+kyilfrog@users.noreply.github.com>
2024-03-29 16:22:31 +0900

    기본 객체들

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -5,11 +5,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.Data;
 
 @Controller
@@ -47,6 +51,23 @@ public class BasicController {
 		 
 		 return "basic/variable";
 	}
+	
+	@GetMapping("/basic-objects")
+	public String basicObjects(HttpSession session, Model model, HttpServletRequest request, HttpServletResponse response) {
+		session.setAttribute("sessionData", "Hello Session");
+		model.addAttribute("request", request);
+		model.addAttribute("response", response);
+		model.addAttribute("servletContext", request.getServletContext());
+		return "basic/basic-objects";
+	}
+	
+	@Component("helloBean")
+	static class HelloBean{
+		public String hello(String data) {
+			return "Hello " + data;
+		}
+	}
+	
 	
 	@Data
 	static class User {
